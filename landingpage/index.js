8f503f621fbc0cebe3a1ec2c0de6531fa619a3e5
SushiInGit <philipp.lars@gmail.com>
2024-09-18 19:14:02 +0200

    -- add clickable elements

--- landingpage/index.js
+++ landingpage/index.js
@@ -1,18 +1,37 @@
+function createClick() {
+    const elementPhone = document.getElementById('phone');
+    elementPhone.style.cursor = "pointer";
+    elementPhone.addEventListener('click', () => redirect('phone'));
+    const elementBank = document.getElementById('bank');
+    elementBank.style.cursor = "pointer";
+    elementBank.addEventListener('click', () => redirect('bank'));
+}
+
+function redirect(click) {
+    if (click === 'phone') {
+        window.location.href = "phone.html";
+    }
+    if (click === 'bank') {
+        alert("Function: Bank Subpoena is WIP.\nComming soon (tm)...");
+        window.location.href = "";
+    }
+}
+createClick();
+
+/////// RND FUN BULLSHIT *smilie*
+
 function updateTime() {
     const timeElement = document.getElementById("user-time");
     const now = new Date();
-    const options = { 
-        hour: '2-digit', 
-        minute: '2-digit', 
-        timeZoneName: 'short' 
+    const options = {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZoneName: 'short'
     };
-    const userLocale = navigator.language; 
+    const userLocale = navigator.language;
     const formattedTime = now.toLocaleTimeString(userLocale, options);
     timeElement.innerHTML = formattedTime;
 }
-updateTime();
-setInterval(updateTime, 1000);
-
 
 function changeIcons() {
     const iconElement = document.getElementById("icon");
@@ -26,7 +45,12 @@ function changeIcons() {
     const randomInterval = Math.floor(Math.random() * 5000) + 1000;
     setTimeout(changeIcons, randomInterval);
 }
+
 changeIcons();
+updateTime();
+setInterval(updateTime, 1000);
+
+
 
 /*
 "Sorry, I was bored and just wanted to mess around a bit... these functions have zero purpose or value, but hey, 'just for the lulz' xD"
