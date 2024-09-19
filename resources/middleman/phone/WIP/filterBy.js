1cb202c3a1d4c7308f41acbcc2ba626200085da3
SushiInGit <98706694+SushiInGit@users.noreply.github.com>
2024-09-19 08:56:21 +0200

    -- remove oldfunctions thats not used anymore -- add MessageStrict filter

--- resources/middleman/phone/WIP/filterBy.js
+++ resources/middleman/phone/WIP/filterBy.js
@@ -2,7 +2,7 @@
 var middleman = middleman ?? {};
 
 middleman.filterBy = function () {
-const groupData = middleman.groupeCommunications.output()
+    const groupData = middleman.groupeCommunications.output()
 
     function findCommunicationsByNumber(data, filter) {
         let output = data.filter(object => {
@@ -19,7 +19,6 @@ const groupData = middleman.groupeCommunications.output()
             const pointer = object.communications[0].Message;
 
             if (pointer !== null) {
-                console.log(pointer);
                 return JSON.stringify(pointer)
                     .toString()
                     .toLowerCase()
@@ -29,16 +28,34 @@ const groupData = middleman.groupeCommunications.output()
         return output;
     }
 
+    function strictFilter(data, filter) {
+        const pointer = data[0].communications;
+        let output = pointer.filter(object => {
+            if (object.Message.includes(filter.toString().toLowerCase()) === true) {
+                console.log("--> ", object.Message);
+                if (pointer !== null) {
+                    return JSON.stringify(pointer)
+                        .toString()
+                        .toLowerCase()
+                        .includes(filter.toString().toLowerCase());
+                }
+            }
+        });
+        return output;
+    };
+
+    ////////// ALL Search
     function findCommunicationsByAll(filter) { // find + remove Dupes + sort it by groupIndex again (if its a number in the text and as phonenumber becouse i cancat it =] )
         const allData = findCommunicationsByNumber(groupData, filter).concat(findCommunicationsByText(groupData, filter));
         const ids = allData.map(({ groupIndex }) => groupIndex);
         const filtered = allData.filter(({ groupIndex }, index) => !ids.includes(groupIndex, index + 1));
-        return filtered.sort(function(a, b) {return (a.groupIndex - b.groupIndex);});;
+        return filtered.sort(function (a, b) { return (a.groupIndex - b.groupIndex); });;
     }
 
     return {
         Number: (filter) => { return findCommunicationsByNumber(groupData, filter) },
         Message: (filter) => { return findCommunicationsByText(groupData, filter) },
+        MessageStrict: (filter) => { return strictFilter(findCommunicationsByText(groupData, filter), filter) },
         All: (filter) => { return findCommunicationsByAll(filter) }
     }
 }();
