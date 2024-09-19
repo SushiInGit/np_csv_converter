020841e83790f185e87eacaefea7712fd694a85d
RYJarnovk <jarnovk@hotmail.com>
2024-09-19 16:44:11 +0200

    Bank upload with some small tweaks to phone and general file handling

---
+++ resources/frontend/js/bank/renderTable.js
@@ -0,0 +1,22 @@
+
+/* THIS WHOLE FILE IS TEMP TO RENDER A TABLE WHILE WE DONT HAVE A DESIGN YET */
+
+window.addEventListener("load", (event) => {
+
+    var table = document.querySelector("#bankRecordsTable");
+    var data = backend.dataController.getData(backend.helpers.getAllSheetTypes().BANKRECORDS);
+
+    data.forEach((row) => {
+        var tr = document.createElement("tr");
+
+        Object.values(row).forEach((item) => {
+            var td = document.createElement("td");
+
+            td.textContent = item;
+
+            tr.appendChild(td);
+        });
+
+        table.appendChild(tr);
+    });
+});
