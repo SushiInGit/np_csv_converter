503337cda42a5fb5314cc44c5d2c7dda9e236c9d
SushiInGit <98706694+SushiInGit@users.noreply.github.com>
2024-10-20 19:09:54 +0200

    --bugfix print

--- resources/middleman/global/printJob.js
+++ resources/middleman/global/printJob.js
@@ -14,15 +14,15 @@ middleman.printJob = function () {
         newWindow.document.write('<link rel="stylesheet" type="text/css" href="resources/frontend/css/phone/logs.css">');
         newWindow.document.write('<link rel="stylesheet" type="text/css" href="resources/frontend/css/phone/print.css">');
         newWindow.document.write('</head><body>');
-        newWindow.document.write('<container><div class="grid-wrapper"><div class="output" style="grid-area: 1/1/5/7"><div class="messages">');
+        newWindow.document.write('<container1><div class="grid-wrapper"><div class="output" style="grid-area: 1/1/5/7"><div class="messages">');
         newWindow.document.write(printContents);
-        newWindow.document.write('</div></div></div></container>');
+        newWindow.document.write('</div><div class="spacer"></div></div></div></container1>');
         newWindow.document.write('</body></html>');
     
         newWindow.document.close(); 
         setTimeout(function() {
-            newWindow.print(); 
-            newWindow.close(); 
+            //newWindow.print(); 
+            //newWindow.close(); 
         }, 1000);
     }
     return {
