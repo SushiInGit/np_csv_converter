8222e5d1343aa24f9fd982f07a4fa741fffa5383
SushiInGit <98706694+SushiInGit@users.noreply.github.com>
2024-10-23 13:12:09 +0200

    -- printJob: Add waiting text and optimize writes/render

--- resources/middleman/global/printJob.js
+++ resources/middleman/global/printJob.js
@@ -29,40 +29,46 @@ middleman.printJob = function () {
           </div>`
 		);
 
-		var newWindow = window.open();
-
-		newWindow.document.write(`
-			<html><head><title>NP Converter Print View</title>
+		var createExport = window.open();
+		createExport.document.write(`
+			<html><head>
+			<title>NP Converter Print View</title>
 			<link rel="preload" href="resources/frontend/css/phone/print.css" as="style" onload="this.rel='stylesheet'">
 			<script defer src="resources/backend/vendors/html2canvas.min.js"></script>
 			</head>
-			`);
-		newWindow.document.write('<body id="capture">');
-		newWindow.document.write('<div class="banner">' + pageDate.head + '</div>');
-		newWindow.document.write(`
-		<div class="help">
-		<hr class="helphr">
-		<center><h2>Exporting Logs with Visibility Controls</h2></center>
-		<div class="helptext">
-			<p>When interacting with logs, you have the ability to manage which entries are visible or hidden. By clicking on a log entry, you can toggle its visibility:</p>
-			<ul>
-				<li><strong>Visible Logs</strong>: Logs are displayed normally, fully opaque and readable.</li>
-				<li><strong>Hidden Logs</strong>: When hidden, the log entries appear in a <span style="opacity: 0.5; text-decoration: line-through;">semi-transparent style with a strikethrough</span>, indicating they are deactivated and will not be included in the export.</li>
-			</ul>
-			<p>This flexible visibility control allows you to customize the logs you want to export, ensuring that only relevant information is included.</p>
+			<div id="hideRender" class="loadingMessage" style="display: none;">
+				<center><h2>Please wait! Image is generating...</h2></center>
 			</div>
-			
-			<div class="buttons">
-				<button class="ok" id="captureButton">Create PNG</button>
+			<body id="capture">
+			<div class="exportBody"><div class="banner">${pageDate.head}</div>
+			<div class="help" id="hideRender">
+				<hr class="helphr" id="hideRender">
+				<center><h2>Exporting Logs with Visibility Controls</h2></center>
+				<div class="helptext">
+					<p>When interacting with logs, you have the ability to manage which entries are visible or hidden. By clicking on a log entry, you can toggle its visibility:</p>
+					<ul>
+						<li><strong>Visible Logs</strong>: Logs are displayed normally, fully opaque and readable.</li>
+						<li><strong>Hidden Logs</strong>: When hidden, the log entries appear in a <span style="opacity: 0.5; text-decoration: line-through;">semi-transparent style with a strikethrough</span>, indicating they are deactivated and will not be included in the export.</li>
+					</ul>
+					<p>This flexible visibility control allows you to customize the logs you want to export, ensuring that only relevant information is included.</p>
+					</div>
+					<div class="buttons">
+						<button class="ok" id="captureButton">Create PNG</button>
+					</div>
+					<hr class="helphr" id="hideRender">
+				</div>
+				<div class="logBody">
+					<div class="output" style="grid-area: 1/1/5/7">
+						<div class="messages">${pageDate.log}</div>
+					</div>
+				</div>
 			</div>
-			<hr class="helphr">
-		</div>
+			<footer id="hideRender">${pageDate.footer}</footer>
+			<script defer src="resources/middleman/global/printJobFunction.js"></script>
+			</body></html>
 		`);
-		newWindow.document.write('<div class="logBody"><div class="output" style="grid-area: 1/1/5/7"><div class="messages">' + pageDate.log + '</div></div></div>');
-		newWindow.document.write('<footer>' + pageDate.footer + '</footer>');
-		newWindow.document.write('<script defer src="resources/middleman/global/printJobFunction.js"></script>');
-		newWindow.document.write('</body></html>');
-		newWindow.document.close();
+
+		createExport.document.close();
 	}
 	return {
 		printJob: printDiv
--- resources/middleman/global/printJobFunction.js
+++ resources/middleman/global/printJobFunction.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-
     document.body.addEventListener('click', function (event) {
         const closestParent = event.target.closest('.date-marker, .message, .call');
 
@@ -10,107 +9,114 @@ document.addEventListener('DOMContentLoaded', function () {
 
     if (typeof html2canvas === 'function') {
         document.getElementById('captureButton').addEventListener('click', function () {
-
-            const help = document.querySelector('.help');
-            const button = document.querySelector('.buttons');
-            const hide = document.querySelectorAll('.hide');
-            const footer = document.querySelector('footer');
-            const npConInfo = document.querySelector('.left.noselect');
-            const npConImg = document.querySelector('.center.noselect');
-
-            const npConImgStyle = window.getComputedStyle(npConImg);
-            const npConImgUrl = npConImgStyle.backgroundImage;
-
-            button.style.display = 'none';
-            help.style.display = 'none';
-            npConImg.style.backgroundImage = 'none';
-            npConInfo.style.opacity = '0';
-            footer.style.display = 'none';
-
-            hide.forEach(function (div) {
-                div.style.display = 'none';
-            });
-
-            html2canvas(document.body, { scale: 1, allowTaint: true, useCORS: true }).then(function (canvas) {
-                const link = document.createElement('a');
-                const desiredWidth = 4096;
-                const desiredHeight = (canvas.height * desiredWidth) / canvas.width; 
-                const resizedCanvas = document.createElement('canvas');
-                resizedCanvas.width = desiredWidth;
-                resizedCanvas.height = desiredHeight;
+            const exportBody = document.querySelector('.exportBody');
+            const loadingMessage = document.querySelector('.loadingMessage');
+            loadingMessage.style.display = 'block';
+            exportBody.id = "blurred-background";
             
-                const ctx = resizedCanvas.getContext('2d');
-                ctx.drawImage(canvas, 0, 0, desiredWidth, desiredHeight);
+            setTimeout(function () {
 
-                const imgData = resizedCanvas.toDataURL('image/png');
-                link.href = imgData;
+                const hide = document.querySelectorAll('.hide');
+                const npConInfo = document.querySelector('.left.noselect');
+                const npConImg = document.querySelector('.center.noselect');
+                const npConImgStyle = window.getComputedStyle(npConImg);
+                const npConImgUrl = npConImgStyle.backgroundImage;
+               
+                npConImg.style.backgroundImage = 'none';
+                npConInfo.style.opacity = '0';
 
-                var newWindow = window.open('', '', 'height=600,width=800');
-                newWindow.document.title = 'NP Converter - Export PNG';
+                hide.forEach(function (div) {
+                    div.style.display = 'none';
+                });
 
-                newWindow.document.body.innerHTML = `
-                <style>
-                    body {
-                        background-color: #ccc;
-                        background: linear-gradient(315deg, #251F2E 0%, #251F2E 100%);
-                    }
-                        
-                    .ok {
-                        padding: 10px 20px;
-                        background-color: #27ae60;
-                        color: white;
-                        border: none;
-                        border-radius: 6px;
-                        cursor: pointer;
+                html2canvas(document.body, {
+                    scale: 1,
+                    allowTaint: true,
+                    useCORS: true,
+                    ignoreElements: function (element) {
+                        return element.id === 'hideRender'; 
                     }
 
-                    .ok:hover {
-                        background-color: #2ecc71;
-                    }
+                }).then(function (canvas) {
+                    const link = document.createElement('a');
+                    const desiredWidth = 4096;
+                    const desiredHeight = (canvas.height * desiredWidth) / canvas.width; 
+                    const resizedCanvas = document.createElement('canvas');
+                    resizedCanvas.width = desiredWidth;
+                    resizedCanvas.height = desiredHeight;
 
-                    .divstyle {
-                        padding: 20px; 
-                        display: flex; 
-                        justify-content: center; 
-                        flex-direction: column; 
-                        align-items: center;
-                    }
-                    
-                    .bordercolor {
-                        border: 5px solid #ccc;
-                    }
-                </style>
-                `;
+                    const ctx = resizedCanvas.getContext('2d');
+                    ctx.drawImage(canvas, 0, 0, desiredWidth, desiredHeight);
 
-                newWindow.document.body.innerHTML += `
-                <div class="divstyle">
-                    <a href="${imgData}" download="np_converter_export.png" style="text-decoration: none;">
-                        <button class="ok">
-                            Download PNG
-                        </button>
-                    </a>  
-                </div>     
-                <div class="divstyle bordercolor">
-                    <img src="${imgData}" alt="NP Converter Screenshot" style="max-width:100%; margin-bottom: 20px;"/>
-                </div>
-                `;
+                    const imgData = resizedCanvas.toDataURL('image/png');
+                    link.href = imgData;
 
-            }).catch(function (error) {
-                console.error('Error capturing the page:', error);
-            });
+                    var nexportWindow = window.open('', '', 'height=600,width=800');
+                    nexportWindow.document.title = 'NP Converter - Export PNG';
 
-            button.style.display = 'flex';
-            help.style.display = 'flex';
-            npConInfo.style.opacity = '1';
-            footer.style.display = 'block';
-            npConImg.style.backgroundImage = npConImgUrl;
+                    nexportWindow.document.body.innerHTML = `
+                    <style>
+                        body {
+                            background-color: #ccc;
+                            background: linear-gradient(315deg, #251F2E 0%, #251F2E 100%);
+                        }
 
-            hide.forEach(function (div) {
-                div.style.display = 'block';
-            });
+                        .ok {
+                            padding: 10px 20px;
+                            background-color: #27ae60;
+                            color: white;
+                            border: none;
+                            border-radius: 6px;
+                            cursor: pointer;
+                        }
+
+                        .ok:hover {
+                            background-color: #2ecc71;
+                        }
+
+                        .divstyle {
+                            padding: 20px; 
+                            display: flex; 
+                            justify-content: center; 
+                            flex-direction: column; 
+                            align-items: center;
+                        }
+
+                        .bordercolor {
+                            border: 5px solid #ccc;
+                        }
+                    </style>
+
+                    <div class="divstyle">
+                        <a href="${imgData}" download="np_converter_export.png" style="text-decoration: none;">
+                            <button class="ok">
+                                Download PNG
+                            </button>
+                        </a>  
+                    </div>     
+                    <div class="divstyle bordercolor">
+                        <img src="${imgData}" alt="NP Converter Screenshot" style="max-width:100%; margin-bottom: 20px;"/>
+                    </div>
+                    `;
+
+                    loadingMessage.style.display = 'none';
+                    exportBody.id = "";
+
+                }).catch(function (error) {
+                    console.error('Error capturing the page:', error);
+                    loadingMessage.style.display = 'none';
+                    exportBody.id = "";
+                });
+                
+                npConInfo.style.opacity = '1';
+                npConImg.style.backgroundImage = npConImgUrl;
+
+                hide.forEach(function (div) {
+                    div.style.display = 'block';
+                });
+
+            }, 100); 
         });
 
-    } else {
-        console.log('html2canvas is not loaded.');
     }
 });
