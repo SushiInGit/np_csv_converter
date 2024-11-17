24f87441486dd6976abe812fbde703656774c430
SushiInGit <98706694+SushiInGit@users.noreply.github.com>
2024-11-17 14:51:52 +0100

    --bank loader and fix loadingtime

--- resources/frontend/js/bank/renderTable.js
+++ resources/frontend/js/bank/renderTable.js
@@ -1,144 +1,125 @@
 var frontend = frontend ?? {};
 
 frontend.renderTable = function (data) {
-    window.dispatchEvent(new Event('beforeunload'));
     return new Promise((resolve, reject) => {
         var table = document.querySelector("#bankRecordsTable");
-
-        // Check if thead already exists, and remove it if needed
-        var existingThead = table.querySelector("thead");
-        if (existingThead) {
-            table.removeChild(existingThead);
+        if (typeof data !== 'object' || Object.keys(data).length === 0) {
+            return;
         }
 
-        var headers = [
-            "ID", "Comment", 
-            "From: Account ID", "From: Account Name", "From: Accounttype", 
-            "To: Account ID", "To: Account Name", "To: Accounttype", 
-            "Type", "Direction", 
-            "Amount", "Amount without Tax",
-            "Tax %", "Tax Amount", 
-            "Tax-Type", "Tax-ID", 
-            `Date (${processTimestamp(Date.now()).timeZone})`
-        ];
+        setTimeout(() => {
+            window.dispatchEvent(new Event('beforeunload'));
+            var progressbar = document.getElementById("progressbar");
+            progressbar.style.display = "flex";
+            var totalRecords = Object.keys(data).length || 100;  
+            var currentProgress = 0;
 
-        var headersClass = [
-            "id", "comment", 
-            "from_account_id", "from_civ_name", "from_account_name", 
-            "to_account_id","to_civ_name", "to_account_name",
-            "type", "direction",
-            "amount", "original_amount",
-            "tax_percentage", "tax_amount", 
-            "tax_type", "tax_id", 
-            "date"
-        ];
+            function updateProgress(value) {
+                var percentage = Math.round((value / totalRecords) * 100);
+                const progressText = document.querySelector('.percent .num h2');
+                progressText.innerHTML = `${percentage}<span>%</span>`;
+              
+                const circle = document.querySelector('.percent svg circle:nth-child(2)');
+                const radius = 70; 
+                const circumference = 2 * Math.PI * radius; 
+                const offset = circumference - (percentage / 100) * circumference; 
+                
+                circle.style.strokeDashoffset = offset;
+            }
 
-        // Create thead
-        var thead = document.createElement("thead");
-        var headerRow = document.createElement("tr");
-        var headerCount = 0;
-        
-        headers.forEach((header) => {
-            var th = document.createElement("th");
-            th.textContent = header;
-            th.className = headersClass[headerCount];
+            var existingThead = table.querySelector("thead");
+            if (existingThead) {
+                table.removeChild(existingThead);
+            }
 
-            headerRow.appendChild(th);
-            headerCount++;
-        });
-        
-        thead.appendChild(headerRow);
-        table.appendChild(thead);
+            var headers = [
+                "ID", "Comment",
+                "From: Account ID", "From: Account Name", "From: Accounttype",
+                "To: Account ID", "To: Account Name", "To: Accounttype",
+                "Type", "Direction",
+                "Amount", "Amount without Tax",
+                "Tax %", "Tax Amount",
+                "Tax-Type", "Tax-ID",
+                `Date (${processTimestamp(Date.now()).timeZone})`
+            ];
 
-        // Create tbody only once
-        var tbody = document.createElement("tbody");
-        table.appendChild(tbody); // Append tbody to the table initially
+            var headersClass = [
+                "id", "comment",
+                "from_account_id", "from_civ_name", "from_account_name",
+                "to_account_id", "to_civ_name", "to_account_name",
+                "type", "direction",
+                "amount", "original_amount",
+                "tax_percentage", "tax_amount",
+                "tax_type", "tax_id",
+                "date"
+            ];
 
-        // Function to process rows in chunks or all at once
-        function processRows(chunkSize = 100, startIndex = 0) {
-            return new Promise((resolve) => {
-                let endIndex = Math.min(startIndex + chunkSize, data.length);
+            var thead = document.createElement("thead");
+            var headerRow = document.createElement("tr");
+            var headerCount = 0;
 
-                for (let i = startIndex; i < endIndex; i++) {
+            headers.forEach((header) => {
+                var th = document.createElement("th");
+                th.textContent = header;
+                th.className = headersClass[headerCount];
+
+                headerRow.appendChild(th);
+                headerCount++;
+            });
+
+            thead.appendChild(headerRow);
+            table.appendChild(thead);
+            frontend.treeselect();
+
+            var tbody = document.createElement("tbody");
+            function populateTableChunk(index = 0, chunkSize = 50) {
+                var keys = Object.keys(data);
+                var end = Math.min(index + chunkSize, data.length);
+                for (let i = index; i < end; i++) {
                     var row = data[i];
                     var tr = document.createElement("tr");
-                    Object.values(row).forEach((item, counter) => {
-                        var td = document.createElement("td");
-                        var headerClass = headers[counter];
-                        var headerClassIndex = headersClass[counter];
 
-                        if (headerClass === "To: Account Name") {
+                    headersClass.forEach((headerClass, counter) => {
+                        var td = document.createElement("td");
+                        var item = row[headerClass];
+
+                        if (headers[counter] === "To: Account Name") {
                             item = middleman.stateAccounts(row.to_account_id, row.to_civ_name);
                         }
 
-                        if (headerClass === "From: Account Name") {
+                        if (headers[counter] === "From: Account Name") {
                             item = middleman.stateAccounts(row.from_account_id, row.from_civ_name);
                         }
 
-                        if (headerClass === `Date (${processTimestamp(Date.now()).timeZone})`) {
-                            var date = new Date(item); // Convert to Date object
-                            td.textContent = `${processTimestamp(date).displayOrder}`
+                        if (headers[counter].includes("Date")) {
+                            var date = new Date(item);
+                            td.textContent = `${processTimestamp(backend.timeConverterOffset.offsetTime(date)).date} ${processTimestamp(backend.timeConverterOffset.offsetTime(date)).time}`;
                         } else {
-                            td.textContent = item;
+                            td.textContent = item ?? "";
                         }
 
-                        td.className = headerClassIndex;
+                        td.className = headerClass;
                         tr.appendChild(td);
                     });
+
                     tbody.appendChild(tr);
                 }
 
-                if (endIndex < data.length) {
-                    setTimeout(() => processRows(chunkSize, endIndex).then(resolve), 0);
+                currentProgress = end;
+                updateProgress(currentProgress);
+
+                if (end < keys.length) {
+                    setTimeout(() => populateTableChunk(end, chunkSize), 0);
                 } else {
-                    resolve(); // Resolve when all rows are processed
+                    progressbar.style.display = "none";
+                    table.appendChild(tbody);
+                    resolve(table); 
+                    frontend.treeselect();
+                    window.dispatchEvent(new Event('load'));
                 }
-                frontend.treeselect();
-            });
-        }
+            }
 
-        // Check if there are less than or equal to 100 rows
-        if (data.length <= 100) {
-            data.forEach((row) => {
-                var tr = document.createElement("tr");
-                Object.values(row).forEach((item, counter) => {
-                    var td = document.createElement("td");
-                    var headerClass = headers[counter];
-                    var headerClassIndex = headersClass[counter];
-
-                    if (headerClass === "To: Account Name") {
-                        item = middleman.stateAccounts(row.to_account_id, row.to_civ_name);
-                    }
-
-                    if (headerClass === "From: Account Name") {
-                        item = middleman.stateAccounts(row.from_account_id, row.from_civ_name);
-                    }
-
-                    if (headerClass === `Date (${processTimestamp(Date.now()).timeZone})`) {
-                        var date = new Date(item); // Convert to Date object
-                        //td.textContent = `${processTimestamp(date).displayOrder}`
-                        //console.log(processTimestamp(date));
-                        td.textContent = `${processTimestamp(backend.timeConverterOffset.offsetTime(date)).date} ${processTimestamp(backend.timeConverterOffset.offsetTime(date)).time } `
-                    } else {
-                        td.textContent = item;
-                    }
-
-                    td.className = headerClassIndex;
-                    tr.appendChild(td);
-                });
-                tbody.appendChild(tr);
-            });
-            resolve(table); // Return the fully constructed table
-            window.dispatchEvent(new Event('load'));
-        } else {
-            // Start processing rows in chunks
-            processRows(100).then(() => {
-                frontend.treeselect();
-                resolve(table); // Return the fully constructed table
-                window.dispatchEvent(new Event('load'));
-            }).catch(reject);
-        }
+            populateTableChunk();
+        }, 0);
     });
 };
-
-
